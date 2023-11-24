10a21ef67d47cce7f2b4dcee8f7b454b89b055f8
Art <121850372+arterxsk@users.noreply.github.com>
2023-11-24 12:29:47 +0800

    update test.py

--- test.py
+++ test.py
@@ -6,13 +6,6 @@ import json
 import uuid
 
 
-try:
-import pycurl
-from io import BytesIO
-except:
-os.system('pip install pycurl')
-import pycurl
-from io import BytesIO
 
 green = '\033[32m'
 blue = '\033[94m'
