74593e6f87742d8218dc6c1968be4d6503be956a
Art <121850372+arterxsk@users.noreply.github.com>
2023-11-27 16:39:07 +0800

    update art.py

--- art.py
+++ art.py
@@ -71,10 +71,10 @@ def main():
     print(f"\n{lg}  [1] ASK FOR CODE\n  [2] EXIT TOOL")
     print(f"{dg}  ————————————————————————————————————————")
     print(f"{lg}  [!] IF YOU ALREADY HAVE CODE ENTER IT.\n      IF YOU DON'T HAVE ONE,{dg} PRESS 1.")
-    gxdsoption1 = input(f"{lg}  [•] ENTER YOUR CODE:{dg} ")
+    gxdsoption1 = input(f"{lg}  [•] ENTER YOUR CODE:{dg} \n")
     for gxdsloading in range(20):
       time.sleep(0.2)
-      sys.stdout.write("\n\r           CHECKING: " + gxdsanmtn3[gxdsloading % len(gxdsanmtn3)])
+      sys.stdout.write("\r           CHECKING: " + gxdsanmtn3[gxdsloading % len(gxdsanmtn3)])
       sys.stdout.flush()
 
     if gxdsoption1 in ["GXDS", "gxds"]:
