a2acbf55ecfd6f550250bc1da587ebcc3140cfaf
Art <121850372+arterxsk@users.noreply.github.com>
2023-11-27 14:04:14 +0800

    update art.py

--- art.py
+++ art.py
@@ -142,9 +142,9 @@ def gxdsfiles(gxdsfiles1):
         f"{rg}  [-] PRESS CTRL+Z TO STOP THE PROCESS. \n"
     )
     print(f"{dg}  ========================================")
-    print(f"{white}  [•] TOTAL ID IN FILE:{yellow} " + tl)
-    print(f"{white}  [•] PASSWORD LIST:{yellow} AUTOMATIC ")
-    print(f"{white}  [•] FILE SAVE IN:{yellow} /sdcard/gxds.txt\n")
+    print(f"{white}  [•] TOTAL ID IN FILE:{lg} " + tl)
+    print(f"{white}  [•] PASSWORD LIST:{lg} AUTOMATIC ")
+    print(f"{white}  [•] FILE SAVE IN:{lg} /sdcard/gxds.txt\n")
     print(f"{dg}  ========================================")
     print(gxdsprnt)
     with ThreadPool (max_workers=120) as feel:
