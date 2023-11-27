c03086736d3c355fdb5281d540f4011be7f9da95
Art <121850372+arterxsk@users.noreply.github.com>
2023-11-27 17:24:59 +0800

    update art.py

--- art.py
+++ art.py
@@ -70,7 +70,8 @@ def gxdslogo():
     
 def main():
     gxdslogo()
-    print(f"\n{yellow}  [-] FILE CLONING\n  [-] VERSION 0.1\n")
+    print(f"{dg}  ————————————————————————————————————————")
+    print(f"\n{yellow}  [-] FILE CLONING VERSION 0.1\n")
     print(f"{dg}  ————————————————————————————————————————")
     print(f"{lg}  [!] IF YOU ALREADY HAVE CODE ENTER IT.\n      IF YOU DON'T HAVE ONE,{rg} PRESS 1.")
     gxdsoption1 = input(f"{lg}  [•] ENTER YOUR CODE:{rc} ")
