774d791c7a1871cb096ed344a1fa18e77011cd37
Art <121850372+arterxsk@users.noreply.github.com>
2023-11-27 10:06:57 +0800

    update art.py

--- art.py
+++ art.py
@@ -66,7 +66,7 @@ def gxdslogo():
     
 def main():
     gxdslogo()
-    print(f"{white}  [1] ASK FOR CODE\n  [2] EXIT TOOL\n")
+    print(f"\n{white}  [1] ASK FOR CODE\n  [2] EXIT TOOL\n")
     print(gxdsprnt)
     print(f"{rg}  [!] IF YOU ALREADY HAVE CODE ENTER IT.\n      IF YOU DON'T HAVE ONE, PRESS 1.")
     gxdsoption1 = input("\033[1;36m  [•] ENTER YOUR CODE: ")
