ab671759049e73b144f2b9ccf96ee0ba205e1b98
Art <121850372+arterxsk@users.noreply.github.com>
2023-11-27 16:40:51 +0800

    update art.py

--- art.py
+++ art.py
@@ -72,6 +72,7 @@ def main():
     print(f"{dg}  ————————————————————————————————————————")
     print(f"{lg}  [!] IF YOU ALREADY HAVE CODE ENTER IT.\n      IF YOU DON'T HAVE ONE,{dg} PRESS 1.")
     gxdsoption1 = input(f"{lg}  [•] ENTER YOUR CODE:{dg} ")
+    print(" ")
     for gxdsloading in range(20):
       time.sleep(0.2)
       sys.stdout.write("\r           CHECKING: " + gxdsanmtn3[gxdsloading % len(gxdsanmtn3)])
