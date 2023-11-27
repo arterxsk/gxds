225c8add9c12fb05a6997245de0f4b0a1e60456b
Art <121850372+arterxsk@users.noreply.github.com>
2023-11-27 15:44:28 +0800

    update art.py

--- art.py
+++ art.py
@@ -248,7 +248,7 @@ oks=[]
 cps=[]
 def file_subb(uid,pwx):
     global oks,loop,cps
-    sys.stdout.write(f"\r{dg}  [GOXDIES] {loop} | {str(len(oks))}");sys.stdout.flush()
+    sys.stdout.write(f"\r{dg}  [CHECKED] {loop} | {str(len(oks))}");sys.stdout.flush()
     session=requests.Session()
     try:
         for ps in pwx:
