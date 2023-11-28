8eaf304b7cddc84697a9786e565efbc1e78570cf
Art <121850372+arterxsk@users.noreply.github.com>
2023-11-28 14:27:00 +0800

    update art.py

--- art.py
+++ art.py
@@ -80,8 +80,8 @@ xp = plat.replace(' ', '').replace('-', '').replace('#', '').replace(':',
 '').replace('.', '').replace(')', '').replace('(', '').replace('?',
 '').replace('=', '').replace('+', '').replace(';', '').replace('*',
 '').replace('_', '').replace('?', '').replace('  ', '')
-spce = "-"
-gxdsid = rndm3+id1+rndm2+rndm1
+spce = ""
+gxdsid = rndm3+id1+spce+rndm2+rndm1
 gxdsAccess = requests.get('https://raw.githubusercontent.com/arterxsk/test/main/access.txt').text
     
 def menu():
