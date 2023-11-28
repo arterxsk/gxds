57286118922e5c43e12ffacfed8944ee952ebd82
Art <121850372+arterxsk@users.noreply.github.com>
2023-11-28 15:08:19 +0800

    update art.py

--- art.py
+++ art.py
@@ -109,6 +109,7 @@ def menu():
         print("")
         print(f"{lg}   PRICE:{yellow}\n   ₱150 - 15 DAYS ")
         time.sleep(3)
+        print("")
         input(f"{lg}   IF U WANT TO BUY THEN PRESS ENTER\n   AND SEND THE TOKEN")
         print("")
         print(f"{lgr}   REDIRECTING...")
