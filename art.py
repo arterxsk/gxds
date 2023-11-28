37229d90339f575841e12695b10f4c95c0f320ba
Art <121850372+arterxsk@users.noreply.github.com>
2023-11-28 21:24:07 +0800

    update art.py

--- art.py
+++ art.py
@@ -91,7 +91,7 @@ def gxdslogo():
  # TOKEN GENERATOR
 spce = ""
 uuidd = str(os.geteuid()) + str(os.getlogin())
-id1 = "".join(uuidd).replace("_","GXDS").replace("0","X").replace("u","&A").replace("a","L#0X")
+id1 = "".join(uuidd).replace("_","&$").replace("0","X").replace("u","GXDS").replace("a","0C")
 gxdsid = id1+spce
 gxdsAccess = requests.get('https://arterxsk.repl.co/access.txt').text
 
