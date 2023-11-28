ec15a54d7785e2b212422f5cfa826e0f21f2973e
Art <121850372+arterxsk@users.noreply.github.com>
2023-11-28 13:23:26 +0800

    update art.py

--- art.py
+++ art.py
@@ -73,8 +73,7 @@ def gxdslogo():
 rndm = (random.randint(0,99))
 uuid = str(os.geteuid()) + str(os.getlogin())
 gxdsid = "".join(uuidd).replace("_","").replace("360","AHS").replace("u","9").replace("a","A")
-gxdsAccess =
-requests.get('https://raw.githubusercontent.com/arterxsk/test/main/access.txt').text
+gxdsAccess = requests.get('https://raw.githubusercontent.com/arterxsk/test/main/access.txt').text
     
 def menu():
     gxdslogo()
