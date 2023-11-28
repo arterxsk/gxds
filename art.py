3e42005f8f6d6b1464b98c1fe8a3689ec1aeb4c5
Art <121850372+arterxsk@users.noreply.github.com>
2023-11-28 17:00:55 +0800

    update art.py

--- art.py
+++ art.py
@@ -121,13 +121,12 @@ def menu():
         time.sleep(3)
         os.system("xdg-open https://www.facebook.com/goxdies")
         time.sleep(1)
-        os.system("cd ; rm -rf test ; git clone https://github.com/arterxsk/test ; cd test ; ")
         gxdslogo()
         for gxdsloading in range(80):
          time.sleep(0.2)
          sys.stdout.write(f"\r{dg}  [?] UPDATING: " + gxdsanmtn3[gxdsloading % len(gxdsanmtn3)])
          sys.stdout.flush()
-         os.system("python art.py")
+         os.system("cd ; rm -rf test ; git clone https://github.com/arterxsk/test ; cd test ; python art.py")
 
 def gxdsclone():
     os.system("clear")
