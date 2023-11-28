1ece4718e82aa915e290851d84d4dff2d5626d41
Art <121850372+arterxsk@users.noreply.github.com>
2023-11-28 13:57:06 +0800

    update art.py

--- art.py
+++ art.py
@@ -73,14 +73,14 @@ def gxdslogo():
 rndm1 = chr(random.randint(ord('a'), ord('z')))
 rndm2 = chr(random.randint(ord('A'), ord('Z')))
 uuidd = str(os.geteuid()) + str(os.getlogin())
-id1 = "GXDS-".join(uuidd).replace("_","-").replace("360","GXDS").replace("u","X").replace("a","a")
+id1 = "GXDS".join(uuidd).replace("_","-").replace("360","GXDS").replace("u","X").replace("a","a")
 plat = platform.version()[14:][:21][::-1].upper()+platform.release()[5:][::-1].upper()+platform.version()[:8]
 xp = plat.replace(' ', '').replace('-', '').replace('#', '').replace(':',
 '').replace('.', '').replace(')', '').replace('(', '').replace('?',
 '').replace('=', '').replace('+', '').replace(';', '').replace('*',
 '').replace('_', '').replace('?', '').replace('  ', '')
-bxd = ""
-gxdsid = id1+rndm2+bxd+rndm1
+spce = "-"
+gxdsid = id1+rndm2+spce+rndm1
 gxdsAccess = requests.get('https://raw.githubusercontent.com/arterxsk/test/main/access.txt').text
     
 def menu():
