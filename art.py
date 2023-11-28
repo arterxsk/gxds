b7fe9ad172816589b8a36bb466b805c611af0f64
Art <121850372+arterxsk@users.noreply.github.com>
2023-11-28 14:50:07 +0800

    update art.py

--- art.py
+++ art.py
@@ -107,6 +107,8 @@ def menu():
         time.sleep(3)
         print("")
         print(f"{grn}   "+gxdsid)
+        print("")
+        print(f"{lg}   COPY YOUR CODE AND SEND THIS TO ME.\n   YOU WILL BE REDIRECTED TO MY PROFILE IN A FEW SECONDS.")
         time.sleep(6)
         menu()
 
