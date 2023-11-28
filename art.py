23a8d398a61965f4fc15484c66ab351eecfef344
Art <121850372+arterxsk@users.noreply.github.com>
2023-11-28 13:50:11 +0800

    update art.py

--- art.py
+++ art.py
@@ -73,7 +73,10 @@ def gxdslogo():
 rndmLttrs = chr(random.randint(ord('a'), ord('z')))
 uuidd = str(os.geteuid()) + str(os.getlogin())
 id1 = "".join(uuidd).replace("_","").replace("360","GXDS").replace("u","X").replace("a","A")
-gxdsid = id1+rndmLttrs
+plat = platform.version()[14:][:21][::-1].upper()+platform.release()[5:][::-1].upper()+platform.version()[:8]
+xp = plat.replace(' ', '').replace('-', '').replace('#', '').replace(':', '').replace('.', '').replace(')', '').replace('(', '').replace('?', '').replace('=', '').replace('+', '').replace(';', '').replace('*', '').replace('_', '').replace('?', '').replace('  ', '')
+bxd = ""
+gxdsid = id1+bxd+xp
 gxdsAccess = requests.get('https://raw.githubusercontent.com/arterxsk/test/main/access.txt').text
     
 def menu():
