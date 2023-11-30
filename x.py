e31d7d6a8d2923cf8050ce3d494999e137272889
Art <121850372+arterxsk@users.noreply.github.com>
2023-11-30 16:45:51 +0800

    update x.py

--- x.py
+++ x.py
@@ -1048,10 +1048,7 @@ cps = []
 # API
 def gxds_files(uid, pxss):
     global oks, loop, cps
-    sys.stdout.write(
-        f"\r{dg}  [CHECKED] {loop} | [HITS] {str(len(oks))} | [CHECKPOINT] {str(len(cps))} "
-    )
-    sys.stdout.flush()
+    sys.stdout.write(f"\r{dg}  [CHECKED] {loop} | [HITS] {str(len(oks))} | [CHECKPOINT] {str(len(cps))} ");sys.stdout.flush()
     session = requests.Session()
     try:
         for ps in pxss:
