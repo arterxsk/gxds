1006b3c6b57bdc1f195dfeb9b67d7139ca14fa83
Art <121850372+arterxsk@users.noreply.github.com>
2023-11-30 15:06:44 +0800

    update x.py

--- x.py
+++ x.py
@@ -1006,7 +1006,6 @@ def gxdsFilesMenu(gxdsfiles):
         for data in gxdsfiles:
             uid = data.split("|")[0]
             pxss = []
-            pxss.append("@gio.lincoln")
             nxme = data.split("|")[1]
             name = nxme.lower()
             try:
