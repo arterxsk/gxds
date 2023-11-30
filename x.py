0644cb6000f8af82728eff66afd92a5aa1f46c1e
Art <121850372+arterxsk@users.noreply.github.com>
2023-11-30 16:37:47 +0800

    update x.py

--- x.py
+++ x.py
@@ -1100,7 +1100,7 @@ def gxds_files(uid, pxss):
                 oks.append(uid)
                 break
             elif "checkpoint" in lxgin:
-                print('\r\r\033[1;31m  [GXDS-X] '+uid+':'+ps+' - '+yxxr(uid))
+                print('\r\r\033[0;31m  [GXDS-✓] '+uid+':'+ps+' - '+yxxr(uid))
                 open("/sdcard/gxds-cp.txt", "a").write(uid + "|" + ps + "\n")
                 cps.append(uid)
                 break
