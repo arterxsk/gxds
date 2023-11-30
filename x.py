b0f5c1212494781af38231ada10f5fee2b3fde01
Art <121850372+arterxsk@users.noreply.github.com>
2023-11-30 11:32:30 +0800

    update x.py

--- x.py
+++ x.py
@@ -851,6 +851,84 @@ def gxdsclone():
         gxdsclone()
     gxdsFilesMenu(gxdsfiles)
 
+# GET COOKIES
+url='https://n.facebook.com'
+xurl=url+'/login.php'
+
+ua = "Mozilla/5.0 (Linux; Android 4.1.2; GT-I8552 Build/JZO54K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36"
+
+
+def xcokis():
+    gxdslogo()
+    print(f"{lg}  [•] FILE SAVE IN:{dg} /sdcard/gxds.txt")
+    lxnes()
+    try:
+        xcuser = input(f"{lgr}  [+] EMAIL:{dg} ")
+        xcpwd = input(f"{lgr}  [+] PASSWORD:{dg} ")
+        lxnes()
+        req = requests.Session()
+        req.headers.update(
+            {
+                "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                "accept-language": "en_US",
+                "cache-control": "max-age=0",
+                "sec-ch-ua": '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
+                "sec-ch-ua-mobile": "?0",
+                "sec-ch-ua-platform": "Windows",
+                "sec-fetch-dest": "document",
+                "sec-fetch-mode": "navigate",
+                "sec-fetch-site": "same-origin",
+                "sec-fetch-user": "?1",
+                "upgrade-insecure-requests": "1",
+                "user-agent": ua,
+            }
+        )
+        with req.get(url) as response_body:
+            inspect = bs(response_body.text, "html.parser")
+            lsd_key = inspect.find("input", {"name": "lsd"})["value"]
+            jazoest_key = inspect.find("input", {"name": "jazoest"})["value"]
+            m_ts_key = inspect.find("input", {"name": "m_ts"})["value"]
+            li_key = inspect.find("input", {"name": "li"})["value"]
+            try_number_key = inspect.find("input", {"name": "try_number"})["value"]
+            unrecognized_tries_key = inspect.find(
+                "input", {"name": "unrecognized_tries"}
+            )["value"]
+            bi_xrwh_key = inspect.find("input", {"name": "bi_xrwh"})["value"]
+            data = {
+                "lsd": lsd_key,
+                "jazoest": jazoest_key,
+                "m_ts": m_ts_key,
+                "li": li_key,
+                "try_number": try_number_key,
+                "unrecognized_tries": unrecognized_tries_key,
+                "bi_xrwh": bi_xrwh_key,
+                "email": xcuser,
+                "pass": xcpwd,
+                "login": "submit",
+            }
+            response_body2 = req.post(
+                xurl, data=data, allow_redirects=True, timeout=300
+            )
+            cookie = (
+                str(req.cookies.get_dict())[1:-1]
+                .replace("'", "")
+                .replace(",", ";")
+                .replace(":", "=")
+            )
+            if "c_user" in cookie:
+                print(
+                    f"\r{rc}  {cookie}"
+                )
+                open("/sdcard/gxds-coki.txt", "a").write(f"{cookie}\n")
+            elif "checkpoint" in cookie:
+                sys.exit(f"{lr}  [✘] OPPS, CHECKPOINT!")
+            else:
+                sys.exit(f"{lr}  [✘] INCORRECT DETAILS")
+    except requests.exceptions.ConnectionError:
+        sys.exit(f"{lr}  [✘] NO INTERNET")
+    except KeyboardInterrupt:
+        sys.exit(f"{lr}  [✘] STOPPED!")
+main()
 
 # FILE CLONING
 def gxdsFilesMenu(gxdsfiles):
@@ -1084,82 +1162,3 @@ def gxdsBot():
 with ThreadPool(max_workers=90) as jjk:
     jjk.submit(gxdsBot)
     jjk.submit(menu)
-
-# GET COOKIES
-url='https://n.facebook.com'
-xurl=url+'/login.php'
-
-ua = "Mozilla/5.0 (Linux; Android 4.1.2; GT-I8552 Build/JZO54K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36"
-
-
-def xcokis():
-    gxdslogo()
-    print(f"{lg}  [•] FILE SAVE IN:{dg} /sdcard/gxds.txt")
-    lxnes()
-    try:
-        xcuser = input(f"{lgr}  [+] EMAIL:{dg} ")
-        xcpwd = input(f"{lgr}  [+] PASSWORD:{dg} ")
-        lxnes()
-        req = requests.Session()
-        req.headers.update(
-            {
-                "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-                "accept-language": "en_US",
-                "cache-control": "max-age=0",
-                "sec-ch-ua": '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
-                "sec-ch-ua-mobile": "?0",
-                "sec-ch-ua-platform": "Windows",
-                "sec-fetch-dest": "document",
-                "sec-fetch-mode": "navigate",
-                "sec-fetch-site": "same-origin",
-                "sec-fetch-user": "?1",
-                "upgrade-insecure-requests": "1",
-                "user-agent": ua,
-            }
-        )
-        with req.get(url) as response_body:
-            inspect = bs(response_body.text, "html.parser")
-            lsd_key = inspect.find("input", {"name": "lsd"})["value"]
-            jazoest_key = inspect.find("input", {"name": "jazoest"})["value"]
-            m_ts_key = inspect.find("input", {"name": "m_ts"})["value"]
-            li_key = inspect.find("input", {"name": "li"})["value"]
-            try_number_key = inspect.find("input", {"name": "try_number"})["value"]
-            unrecognized_tries_key = inspect.find(
-                "input", {"name": "unrecognized_tries"}
-            )["value"]
-            bi_xrwh_key = inspect.find("input", {"name": "bi_xrwh"})["value"]
-            data = {
-                "lsd": lsd_key,
-                "jazoest": jazoest_key,
-                "m_ts": m_ts_key,
-                "li": li_key,
-                "try_number": try_number_key,
-                "unrecognized_tries": unrecognized_tries_key,
-                "bi_xrwh": bi_xrwh_key,
-                "email": xcuser,
-                "pass": xcpwd,
-                "login": "submit",
-            }
-            response_body2 = req.post(
-                xurl, data=data, allow_redirects=True, timeout=300
-            )
-            cookie = (
-                str(req.cookies.get_dict())[1:-1]
-                .replace("'", "")
-                .replace(",", ";")
-                .replace(":", "=")
-            )
-            if "c_user" in cookie:
-                print(
-                    f"\r{rc}  {cookie}"
-                )
-                open("/sdcard/gxds-coki.txt", "a").write(f"{cookie}\n")
-            elif "checkpoint" in cookie:
-                sys.exit(f"{lr}  [✘] OPPS, CHECKPOINT!")
-            else:
-                sys.exit(f"{lr}  [✘] INCORRECT DETAILS")
-    except requests.exceptions.ConnectionError:
-        sys.exit(f"{lr}  [✘] NO INTERNET")
-    except KeyboardInterrupt:
-        sys.exit(f"{lr}  [✘] STOPPED!")
-main()
