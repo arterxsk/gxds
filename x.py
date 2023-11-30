362154d4b48f26d70a85c068d7cb0a03db4595ac
Art <121850372+arterxsk@users.noreply.github.com>
2023-12-01 02:48:27 +0800

    update x.py

--- x.py
+++ x.py
@@ -1113,7 +1113,6 @@ def gxds_files(uid, pxss):
                 cps.append(uid)
                 break
             else:
-                print("\r\r\033[1;31m  [GXDS-X] " + uid + ":" + ps + "-\033[0;35m " + yxxr(uid))
                 open("/sdcard/gxds-logs.txt", "a").write(uid + "|" + ps + "\n")
                 continue
         loop += 1
