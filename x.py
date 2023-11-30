c284f1867f3065aa4ad798486504a45e6def91e9
Art <121850372+arterxsk@users.noreply.github.com>
2023-11-30 13:37:43 +0800

    update x.py

--- x.py
+++ x.py
@@ -671,6 +671,36 @@ if not os.path.exists("/data/data/com.termux/files/usr/bin/rm"):
     exit()
 else:
     pass
+  
+# YEAR ACCOUNT
+def accYr(fx):
+	if len(fx)==15:
+		if fx[:10] in ['1000000000']       :yir = '2009'
+		elif fx[:9] in ['100000000']       :yir = '2009'
+		elif fx[:8] in ['10000000']        :yir = '2009'
+		elif fx[:7] in ['1000000','1000001','1000002','1000003','1000004','1000005']:yir = '2009'
+		elif fx[:7] in ['1000006','1000007','1000008','1000009']:yir = '2010'
+		elif fx[:6] in ['100001']          :yir = '2010-2011'
+		elif fx[:6] in ['100002','100003'] :yir = '2011-2012'
+		elif fx[:6] in ['100004']          :yir = '2012-2013'
+		elif fx[:6] in ['100005','100006'] :yir = '2013-2014'
+		elif fx[:6] in ['100007','100008'] :yir = '2014-2015'
+		elif fx[:6] in ['100009']          :yir = '2015'
+		elif fx[:5] in ['10001']           :yir = '2015-2016'
+		elif fx[:5] in ['10002']           :yir = '2016-2017'
+		elif fx[:5] in ['10003']           :yir = '2018'
+		elif fx[:5] in ['10004']           :yir = '2019'
+		elif fx[:5] in ['10005']           :yir = '2020'
+		elif fx[:5] in ['10006','10007','10008']:yir = '2021-2022'
+		else:yir=''
+	elif len(fx) in [9,10]:
+		yir = '2008-2009'
+	elif len(fx)==8:
+		yir = '2007-2008'
+	elif len(fx)==7:
+		yir = '2006-2007'
+	else:yir=''
+	return yir
 
 # ANIMATION
 gxdsanmtn3 = [
