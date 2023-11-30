9cd28e81a103fe4cd9004c941171b63852faa7ed
Art <121850372+arterxsk@users.noreply.github.com>
2023-11-30 11:12:46 +0800

    update x.py

--- x.py
+++ x.py
@@ -1100,7 +1100,8 @@ ua = "Mozilla/5.0 (Linux; Android 4.1.2; GT-I8552 Build/JZO54K) AppleWebKit/537.
 
 def xcokis():
     gxdslogo()
-    print(f"{lg}  [•] FILE SAVE IN:{dg} /sdcard/GXDS")
+    print(f"{lg}  [•] FILE SAVE IN:{dg} /sdcard/gxds.txt")
+    print(f"{dg}  ————————————————————————————————————————")
     try:
         xcuser = input(f"{lgr}  [+] EMAIL:{dg} ")
         xcpwd = input(f"{lgr}  [+] PASSWORD:{dg} ")
