bc0c97a59d3aa1c2a1df326fcb9369f6a04159d2
Art <121850372+arterxsk@users.noreply.github.com>
2023-12-01 15:27:58 +0800

    update x.py

--- x.py
+++ x.py
@@ -530,7 +530,7 @@ def gxds_files(uid, pxss):
             }
             update = {
                 "authority": f"{fb}.facebook.com",
-                "method": "POST",
+                "method": "GET",
                 "scheme": "https",
                 "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.8",
                 "accept-encoding": "gzip, deflate, br",
